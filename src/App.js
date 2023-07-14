1c38410224d3f5785af86648b957aa870e7ca8ab
Shailesh Singh <shaileshsiingh@gmail.com>
2023-07-14 14:17:23 +0530

    added cart with react portal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Header from './components/Layouts/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <div>
+      <Cart/>
       <Header />
       <main>
         <Meals/>
