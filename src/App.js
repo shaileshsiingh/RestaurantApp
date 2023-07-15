2ebf2383508d9c6dd760229c59bf42dd84b7e129
Shailesh Singh <shaileshsiingh@gmail.com>
2023-07-15 16:25:41 +0530

    cart can open and close

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 import Header from './components/Layouts/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+const [cartIsShown,setCartIsShown] = useState(false)
+
+const showCartHandler = ()=>{
+  setCartIsShown(true)
+}
+
+const hideCartHandler = ()=>{
+  setCartIsShown(false)
+}
+
   return (
     <div>
-      <Cart/>
-      <Header />
+      {cartIsShown && <Cart onClose = {hideCartHandler}/>}
+      <Header onShowCart = {showCartHandler}/>
       <main>
         <Meals/>
       </main>
