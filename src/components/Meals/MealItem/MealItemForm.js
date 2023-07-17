f0018a2ab2f6fd6cf327646e76b93d24ceb0698c
Shailesh Singh <shaileshsiingh@gmail.com>
2023-07-17 23:54:15 +0530

    updates the cart when click add button

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,39 +1,33 @@
-import React, { useContext } from 'react';
-import classes from './MealItemForm.module.css';
-import Input from '../../UI/Input';
-import CartContext from '../../../store/cart-context';
+import React,{useContext} from 'react'
+import classes from './MealItemForm.module.css'
+import Input from '../../UI/Input'
+import CartContext from '../../../store/cart-context'
 
 function MealItemForm(props) {
-  const cartCtx = useContext(CartContext);
-
-  const addItemCart = (event) => {
-    event.preventDefault();
-    const quantity = document.getElementById('amount_' + props.id).value;
-    const newItem = {
-      ...props.item,
-      quantity: +quantity // Convert quantity to a number
-    };
-    cartCtx.addItem(newItem);
-    console.log(newItem);
-  };
-
+const cartCtx = useContext(CartContext)
+const addItemCart = (event) => {
+  event.preventDefault()
+  //update the cortcontext.items
+  const quantity = document.getElementById('amount_' + props.id).value
+  cartCtx.addItem({...props.item, quantity : quantity})
+  console.log(props.item)
+}
+  
   return (
     <form className={classes.form}>
       {console.log(cartCtx.items)}
-      <Input
-        label="Amount"
-        input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1'
-        }}
-      />
-      <button onClick={addItemCart}>+ Add</button>
+        <Input label = 'Amount' input = {{
+            id : 'amount_' + props.id,
+            type : 'number',
+            min : '1',
+            max : '5',
+            step : '1',
+            defaultValue : '1'
+
+        }}/>
+        <button onClick={addItemCart} >+ Add</button>
     </form>
-  );
+  )
 }
 
-export default MealItemForm;
+export default MealItemForm
